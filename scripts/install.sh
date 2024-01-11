ff2a1daa0b52e823226196c51e6edc070b02500a
demuma <92734505+demuma@users.noreply.github.com>
2024-01-11 17:16:18 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install ros-noetic-dwa-local-planner ros-noetic-global-planner ros-noetic-base-local-planner ros-noetic-pointcloud-to-laserscan ros-noetic-amcl ros-noetic-carla-msgs -y &&
+sudo apt-get install ros-noetic-dwa-local-planner ros-noetic-global-planner ros-noetic-teb-local-planner ros-noetic-base-local-planner ros-noetic-pointcloud-to-laserscan ros-noetic-amcl ros-noetic-carla-msgs -y &&
 cd ~/carla_ws/src/
 git clone https://github.com/carla-simulator/ros-bridge
 cd ros-bridge
