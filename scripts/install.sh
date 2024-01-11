199c6474f34d307c5781cbcdaa98e0c4fdcaa004
demuma <max.demuirier@gmx.de>
2024-01-11 13:42:40 +0100

    changed installation script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,2 +1,2 @@
 sudo apt-get update
-sudo apt-get install ros-noetic-dwa-local-planner ros-noetic-global-planner ros-noetic-base-local-planner ros-noetic-pointcloud-to-laserscan ros-noetic-amcl
+sudo apt-get install ros-noetic-dwa-local-planner ros-noetic-global-planner ros-noetic-base-local-planner ros-noetic-pointcloud-to-laserscan ros-noetic-amcl -y
